cfbb9fefacd30a5616fc6e86a6c19a77bf6fcc9b
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-11-09 22:00:19 -0500

    updates

--- application/main.py
+++ application/main.py
@@ -1,9 +1,45 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print BASE_DIR
+UPLOAD_FOLDER = '/srv/www/servies.bohdananderson.com/public_html/images'
+ALLOWED_EXTENSIONS = set(['jpg','jpeg'])#set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
 
 @app.route("/")
 def hello():
 	return "Hello World!"
+
+
 
 if __name__ == "__main__":
 	app.run()
